bfde63bb36daf3f8333b9253f719a52b4c542d6b
rhcsky <mangusn1@gmail.com>
2020-09-14 18:17:07 +0900

    Make basic frame of Main Activity

---
+++ app/src/main/java/com/example/myapplication/Login.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Login extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.mypage, container, false);
+
+        WebView webView = (WebView) view.findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://ec2-54-180-149-162.ap-northeast-2.compute.amazonaws.com:5000");
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,20 +1,67 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
-    private WebView webView;
+    private Button btn_home;
+    private Button btn_school;
+    private Button btn_board;
+    private Button btn_simulation;
+    private Button btn_mypage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        btn_home = findViewById(R.id.btn_home);
+        btn_school = findViewById(R.id.btn_school);
+        btn_board = findViewById(R.id.btn_board);
+        btn_simulation = findViewById(R.id.btn_simulation);
+        btn_mypage = findViewById(R.id.btn_mypage);
 
-        webView = (WebView) findViewById(R.id.webview);
-        webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("http://ec2-54-180-149-162.ap-northeast-2.compute.amazonaws.com:5000");
+
+        btn_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new Home());
+            }
+        });
+        btn_school.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new School());
+            }
+        });
+        btn_board.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new Board());
+            }
+        });
+        btn_simulation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new Simulation());
+            }
+        });
+        btn_mypage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new Mypage());
+            }
+        });
+    }
+
+    public void replaceFragment(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_main,fragment);
+        transaction.commit();
     }
 }
+
+
+
