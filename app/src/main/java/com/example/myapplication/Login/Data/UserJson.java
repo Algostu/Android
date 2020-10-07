2cf5e681fddbb1af10ed42e1568e531b70662079
HanKyul Kang <consistant1y@ajou.ac.kr>
2020-10-07 11:59:16 +0900

    Change User Info Json
    User info json object was originally contain userid, schoolid, regionid, but these information is not need to store in user anymore, so user wil have instead of credential info, school name, region name, gender, town name, etc.

--- app/src/main/java/com/example/myapplication/Login/Data/UserJson.java
+++ app/src/main/java/com/example/myapplication/Login/Data/UserJson.java
@@ -4,14 +4,20 @@ import java.io.Serializable;
 
 @SuppressWarnings("serial")
 public class UserJson implements Serializable {
-    public long userID;
-    public int schoolID;
-    public int regionID;
+    // school information
+    public String regionName;
+    public String townName;
+    public String schoolName;
+    public String schoolGender;
     public String email;
+    public String contact;
+    public String homePage;
+    // user information
     public int gender;
     public int age;
     public int grade;
     public String nickName;
+    // return value
     public String status;
 
     public String getNickName() {
--- app/src/main/java/com/example/myapplication/Login/startUpActivity.java
+++ app/src/main/java/com/example/myapplication/Login/startUpActivity.java
@@ -69,7 +69,6 @@ public class startUpActivity extends AppCompatActivity {
                     String expDateStr = format.format(c.getTime());
                     SharedPreferences.Editor edit = sharedPref.edit();
                     edit.putString("expDate", expDateStr);
-                    edit.commit();
                     // load user object and pass it to main Activity
                     Gson gson = new Gson();
                     String json = sharedPref.getString("userObject", "");
