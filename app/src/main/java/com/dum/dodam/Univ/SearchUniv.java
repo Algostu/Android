dafd79ccf032a84f089ed85e4f9e6310946f282a
HanKyul Kang <consistant1y@ajou.ac.kr>
2020-11-10 15:02:17 +0900

    Fix bugs
    Prevent double click in short time and make onFocuse claer

--- app/src/main/java/com/dum/dodam/Univ/SearchUniv.java
+++ app/src/main/java/com/dum/dodam/Univ/SearchUniv.java
@@ -59,6 +59,8 @@ public class SearchUniv extends Fragment {
                 InputMethodManager imm = (InputMethodManager) ((MainActivity) getActivity()).getSystemService(INPUT_METHOD_SERVICE);
                 imm.hideSoftInputFromWindow(et_input.getWindowToken(), 0);
                 ((MainActivity) getActivity()).replaceFragmentFull(new Univ(collage));
+                list.clear();
+                et_input.clearFocus();
             }
         });
         et_input.addTextChangedListener(new TextWatcher() {
