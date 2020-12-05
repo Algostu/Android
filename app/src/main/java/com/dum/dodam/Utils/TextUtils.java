de3fc9d8bfb0d6a65f19b8d3bd95f933e9c5837e
rhcsky <mangusn1@gmail.com>
2020-12-06 04:28:55 +0900

    Change font size

--- app/src/main/java/com/dum/dodam/Utils/TextUtils.java
+++ app/src/main/java/com/dum/dodam/Utils/TextUtils.java
@@ -7,7 +7,7 @@ import android.text.style.ForegroundColorSpan;
 import android.widget.TextView;
 
 public class TextUtils {
-    public static TextView setColorInPartitial(String string, int start, int end, String color, TextView textView){
+    public static TextView setColorInPartitial(String string, int start, int end, String color, TextView textView) {
         SpannableStringBuilder builder = new SpannableStringBuilder(string);
         builder.setSpan(new ForegroundColorSpan(Color.parseColor(color)), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         textView.append(builder);
